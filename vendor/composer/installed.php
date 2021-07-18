373381258003fb405ca66f1fcfa1753ee7ff2836
RuslanPeka <pekaruslan2009@ya.ru>
2021-07-18 19:00:45 +0300

    Variant_1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '19370ae770aa32442223d8cb7f96f77e425b09eb',
+        'name' => 'my_project/my_project',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'my_project/my_project' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '19370ae770aa32442223d8cb7f96f77e425b09eb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
